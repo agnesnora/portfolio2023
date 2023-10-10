04228704a3e4ad1da1a89b6ffe31139e025c7e57
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-10-10 15:03:08 +0200

    wigh slash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "portfolio2023",
+  base: "/portfolio2023",
   // base: "/portfolio2023",
 });
