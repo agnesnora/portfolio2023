da88d08f2e451b8d39c9229f8ce6ccde4f073619
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-10-10 14:26:18 +0200

    f

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "/portfolio2023",
-  base: "/portfolio2023",
+  // base: "/portfolio2023",
 });
