7ec54116df0546de61eecd1e74f2d1385a1c5fab
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-11-27 15:40:13 +0100

    colorreact

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ createServer({
       description:
         "This Quizzical app was a solo project as part of the React Basics Module of the Scrimba Frontend Career Path. Built from scratch using React, CSS and API. Own design with surprise confetti. ",
       imageUrl: "images/Quizzical.png",
-      type: ["React", "CSS", "HTML5"],
+      type: ["React", "CSS", "Responsive"],
       live: "https://agnesnora.github.io/quizzical/",
       code: "https://github.com/agnesnora/quizzical",
     });
@@ -36,7 +36,7 @@ createServer({
       code: "https://github.com/agnesnora/tindogReact",
 
       description:
-        "Tindog is a dating app for dogs, which was a solo project of Scrimba Career Path. The main feature was to pop up a like/nope badge on the dog's image after clicking on the like or dislike buttons. I expanded it with a chat feature, and the users can also save the liked dogs in their profile",
+        "Tindog is a dating app for dogs, which was a solo project of Scrimba Career Path. The main feature was to pop up a like/nope badge on the dog's image after clicking on the like or dislike buttons. I expanded it with a chat feature, and the users can also save the liked dogs in their profile.",
 
       type: ["React", "Responsive", "HTML5"],
     });
@@ -59,18 +59,18 @@ createServer({
       description:
         "The Color Scheme Generator was a solo project as part of the Working with APIs Module of the Scrimba Career Path. The user can choose 'seed color' and color scheme mode. Clicking the button makes a request to the Color API to get a color scheme.",
       imageUrl: "images/Color.png",
-      type: ["Javascript", "API", "CSS", "HTML5"],
-      live: "https://agnesnora.github.io/color-scheme-generator/",
-      code: "https://github.com/agnesnora/color-scheme-generator",
+      type: ["React", "API", "CSS"],
+      live: "https://coloragnesnora.netlify.app",
+      code: "https://github.com/agnesnora/colorReact",
     });
 
     server.create("project", {
       id: "6",
       name: "Component library",
-      description:
-        "This Component Library was a solo project as part of the Advanced React Module of the Scrimba Career Path. Built from scratch, creating reusable components in React. Design from a given Figma file. ",
       imageUrl: "images/Component.png",
-      type: ["React", "CSS", "HTML5"],
+      description:
+        "This Component Library was a solo project as part of the Advanced React Module of the Scrimba Career Path. Built from scratch, creating reusable components in React. Own design.",
+      type: ["React", "CSS", "Responsive"],
       live: "https://clever-kitten-80ba4d.netlify.app/",
       code: "https://github.com/agnesnora/componentLibrary",
     });
